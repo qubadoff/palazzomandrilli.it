4296a737700dee4f0789bd89c51497fed31fe66d
qubadoff <admin@burncode.org>
2025-06-07 01:45:14 +0400

    second

--- resources/views/Frontend/layouts/app.blade.php
+++ resources/views/Frontend/layouts/app.blade.php
@@ -164,7 +164,6 @@
 <script src="{{ asset("") }}assets/js/wow.js"></script>
 <script src="{{ asset("") }}assets/js/appear.js"></script>
 <script src="{{ asset("") }}assets/js/isotope.js"></script>
-<script src="{{ asset("") }}assets/js/jquery.fancybox.js"></script>
 <script src="{{ asset("") }}assets/js/validate.js"></script>
 <script src="{{ asset("") }}assets/js/appear.js"></script>
 <script src="{{ asset("") }}assets/js/mixitup.js"></script>
