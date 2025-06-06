140a8b8c021b2ffc607e506ff2d8aa79bcda6f82
qubadoff <admin@burncode.org>
2025-06-07 01:25:19 +0400

    second

--- resources/views/Frontend/layouts/includes/header.blade.php
+++ resources/views/Frontend/layouts/includes/header.blade.php
@@ -42,14 +42,14 @@
     </ul>
 
     <!-- Search Box -->
-    <div class="search-box">
-        <form method="post" action="#">
-            <div class="form-group">
-                <input type="search" name="search-field" value="" placeholder="Search" required="">
-                <button type="submit"><span class="fa fa-search"></span></button>
-            </div>
-        </form>
-    </div>
+{{--    <div class="search-box">--}}
+{{--        <form method="post" action="#">--}}
+{{--            <div class="form-group">--}}
+{{--                <input type="search" name="search-field" value="" placeholder="Search" required="">--}}
+{{--                <button type="submit"><span class="fa fa-search"></span></button>--}}
+{{--            </div>--}}
+{{--        </form>--}}
+{{--    </div>--}}
 
 </section>
 <!-- End / Hidden Bar -->
